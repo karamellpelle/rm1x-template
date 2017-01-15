b7141ddf5af8c4614a81d683ea34bf1050872208
Carl Joachim Svenn <karamellpelle@hotmail.com>
2017-01-16 00:02:38 +0100

    fixed names in kits. pages not fixed fully.

--- source/Main.hs
+++ source/Main.hs
@@ -1,4 +1,67 @@
+--    rm1x-template: make keymaps for Yamaha Rm1X
+--    Copyright (C) 2017  karamellpelle@hotmail.com
+--
+--    This program is free software; you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation; either version 2 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License along
+--    with this program; if not, write to the Free Software Foundation, Inc.,
+--    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
 module Main where
 
+import System.Environment
+import System.Exit
+import Control.Monad
+import Template
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs
+    template <- getDataFileName "template.svg"
+   
+    path  <- getOption "input"
+    --path' <- getOption "output"
+    
+    map <- midiMap path
+    
+    forM_ map $ \(x, y) -> putStrLn $ x ++ " -> " ++ y
+
+
+
+--------------------------------------------------------------------------------
+--  
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName path =
+    return $ "data/" ++ path
+
+--------------------------------------------------------------------------------
+--  
+
+getOption :: String -> IO String
+getOption opt = do
+    args <- getArgs
+    case find args opt of 
+        Nothing     -> die $ "no --" ++ opt ++ " option given"
+        Just ""     -> die $ "empty --" ++ opt ++ " option"
+        Just str    -> return str
+
+    where
+      find [] opt     = Nothing
+      find (a:as) opt =
+          if ( "--" ++ opt == a )
+            then case as of 
+                    (a':as') -> Just a'
+                    _        -> Just ""
+            else find as opt
+
+
+
--- source/Template.hs
+++ source/Template.hs
@@ -1,3 +1,26 @@
+--    rm1x-template: make keymaps for Yamaha Rm1X
+--    Copyright (C) 2017  karamellpelle@hotmail.com
+--
+--    This program is free software; you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation; either version 2 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License along
+--    with this program; if not, write to the Free Software Foundation, Inc.,
+--    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+--
+module Template
+  (
+    midiMap
+  ) where
+
+
 import Data.List
 import Data.String
 
@@ -6,9 +29,10 @@ midiMap :: FilePath -> IO [(String, String)]
 midiMap path =
     readFile path >>= \file ->
         return $ makeMap $ lines file
-        where
-            makeMap ls = foldr step [] ls
-            step line xs =
-                case words line of 
-                    (w0:ws@(w1:ws2)) -> (w0, unwords ws) : xs   -- line has at least 2 words
-                    _                -> xs                      -- do nothing and keep rollin'
+
+    where
+        makeMap ls = foldr step [] ls
+        step line xs =
+            case words line of 
+                (w0:ws@(w1:ws2)) -> (w0, unwords ws) : xs   -- line has at least 2 words
+                _                -> xs                      -- do nothing and keep rollin'
